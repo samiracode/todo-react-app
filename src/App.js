3f655f126016d9b9b782a2e16b1cfe8d8f6de758
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-10 22:22:26 +0200

    Form component set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react';
 import './App.css';
+// Importing components
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Samira's Todo List</h1>
+      </header>¨
+      <Form />
+      
     </div>
   );
 }

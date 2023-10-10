a13e4ec1ffe40dc8118c9b43661052041999d70b
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-10 22:33:09 +0200

    TodoList component set up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 // Importing components
 import Form from './components/Form';
+import TodoList from './components/TodoLIst';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <h1>Samira's Todo List</h1>
       </header>¨
       <Form />
+      <TodoList />
       
     </div>
   );

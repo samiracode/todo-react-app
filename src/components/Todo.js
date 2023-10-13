0d00f72087711c60e1bb01b731625f5367d76b25
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-13 22:14:35 +0200

    fas fa-check & fa-trash

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Todo = () => {
+    return (
+       <div className="todo">
+        <li className="item">Hejjjj</li>
+        <button className="fas fa-check"></button>
+        <button className="fas fa-trash"></button>
+       </div>
+    );
+}
+
+
+export default Todo;

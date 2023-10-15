0e985e05c2d673f4b1132358f03cb3660b29ce7a
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-15 21:08:49 +0200

    background color

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Samira's Todo List</h1>
+        <h1>Awsome Todo List</h1>
       </header>¨
       <Form 
         todos={todos} 
